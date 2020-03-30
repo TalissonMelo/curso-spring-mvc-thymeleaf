978f110003831bf5c149529cb48c4bf882fd1ea5
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-30 09:08:31 -0300

    Projeto inicializado

---
+++ src/main/java/com/talissonmelo/springmvccobranca/SpringMvcCobrancaApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.springmvccobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCobrancaApplication.class, args);
+	}
+
+}
