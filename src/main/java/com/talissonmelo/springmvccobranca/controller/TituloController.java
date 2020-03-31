3faba48c762cc59acd0ef09212128aeaf0efd449
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-31 09:47:52 -0300

    Mostrando mensagem de sucesso

--- src/main/java/com/talissonmelo/springmvccobranca/controller/TituloController.java
+++ src/main/java/com/talissonmelo/springmvccobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.talissonmelo.springmvccobranca.model.Titulo;
 import com.talissonmelo.springmvccobranca.repository.TitulosRepository;
@@ -11,22 +12,21 @@ import com.talissonmelo.springmvccobranca.repository.TitulosRepository;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private TitulosRepository repository;
-	
+
 	@RequestMapping("/cadastro")
 	public String cadastro() {
 		return "CadastroTitulo";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-
-		System.out.println("Título Descrição " + titulo.getDescricao());
-
+	public ModelAndView salvar(Titulo titulo) {
 		repository.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Título : " + titulo.getDescricao() + ", cadastrado com sucesso!.");
+		return mv;
 	}
 
 }
