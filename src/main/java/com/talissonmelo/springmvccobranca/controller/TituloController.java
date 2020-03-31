c0c15a1ca6dfd3666f100a6a9d3660e277031ba9
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-31 10:09:58 -0300

    Montando StatusTitulo dinamicamente

--- src/main/java/com/talissonmelo/springmvccobranca/controller/TituloController.java
+++ src/main/java/com/talissonmelo/springmvccobranca/controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.talissonmelo.springmvccobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.talissonmelo.springmvccobranca.model.Titulo;
+import com.talissonmelo.springmvccobranca.model.enums.StatusTitulo;
 import com.talissonmelo.springmvccobranca.repository.TitulosRepository;
 
 @Controller
@@ -17,8 +22,9 @@ public class TituloController {
 	private TitulosRepository repository;
 
 	@RequestMapping("/cadastro")
-	public String cadastro() {
-		return "CadastroTitulo";
+	public ModelAndView cadastro() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -27,6 +33,11 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título : " + titulo.getDescricao() + ", cadastrado com sucesso!.");
 		return mv;
+	}
+	
+	@ModelAttribute("statusTitulos")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }
